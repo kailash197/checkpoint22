2be958a79f9b99e6fb79ca5b01baabdebd1aeed5
Kailash Khadka <k.khadka343@gmail.com>
2025-05-07 23:43:32 -0400

    Image3: Firmware Bringup in Tortoisebot

---
+++ ros2_real/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# source underlay
+source /opt/ros/${ROS_DISTRO}/setup.bash
+source ~/ros2_ws/install/setup.bash
+
+# Replace current shell with the command passed as arguments to ensure proper signal handling
+exec "$@"
